22c2cf1dd94f433ddd57ec765a397dc495a189c9
rga6303 <60489846+rga6303@users.noreply.github.com>
2020-03-09 13:50:01 +1300

    Dtic

---
+++ untitled-5.py
@@ -0,0 +1,13 @@
+'''this task is to use mutiple variables at once through Madlibs, 10 feb ''' 
+#take players input to make variables
+print("welcome to my Madlib program")
+number= int(input(" select a number smaller than 3650")) 
+hybrid= input( "selct a hybrid animal")
+location= input("select a location")
+weakness= input("select a type of food")
+
+
+
+
+#makes the story
+print("this is a warning in" , number, "days there will be an attack from a giant", hybrid , "the attack will destroy", location, "they only have one weakness", weakness, "get all of this item to save us all")
